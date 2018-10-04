e16bef3b7a565b49c2ca11925af1abd6c48230b1
Mario Jr <mariogasparoni@gmail.com>
2018-10-04 16:16:09 -0300

    Update do CAPS_DETAILS (more useful info)
    We now generate PNG graphs, since it is compatible with the option
    CAPS_DETAILS

--- gen-svg.sh
+++ gen-svg.sh
@@ -4,11 +4,13 @@
 set -e
 DATE=`date +'%Y%m%d%H%M%S'`
 FILENAME='dotgraph_'$DATE
+OUTPUT_FORMAT="png"
+
 if ! [ -x "$(command -v dot)" ]
 then
   echo "error: Graphviz not found";
   exit 1;
 fi
 
-node server > $FILENAME'.dot' && dot -Tsvg $FILENAME'.dot' -o $FILENAME'.svg'
-echo 'Successfully generated '$FILENAME'.svg'
+node server > $FILENAME'.dot' && dot -T$OUTPUT_FORMAT $FILENAME'.dot' -o $FILENAME'.'$OUTPUT_FORMAT
+echo 'Successfully generated '$FILENAME'.'$OUTPUT_FORMAT
